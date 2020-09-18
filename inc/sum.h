a3dded8fa8cef759e723219fa2a08e7c2dfbc092
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 22:23:09 +0530

    Update sum.h

--- inc/sum.h
+++ inc/sum.h
@@ -2,6 +2,8 @@
 #define __SUM_H__
 
 int sum(int number1, int number2);
-
-
+int diff(int number1, int number2);
+int mul(int number1, int number2);
+int div(int number1, int number2);
+int mod(int number1, int number2);
 #endif
