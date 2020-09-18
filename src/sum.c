0867f91cfeeb0e9090e9f6d6dc539497570edf8d
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 22:24:19 +0530

    Update sum.c

--- src/sum.c
+++ src/sum.c
@@ -2,3 +2,19 @@ int sum(int number1, int number2)
 	{
 		return number1 + number2;
 	}
+int diff(int number1, int number2)
+	{
+		return number1 - number2;
+	}
+int mul(int number1, int number2)
+	{
+		return number1 * number2;
+	}
+int div(int number1, int number2)
+	{
+		return number1 / number2;
+	}
+int mod(int number1, int number2)
+	{
+		return number1 % number2;
+	}
