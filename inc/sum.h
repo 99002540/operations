4e4de8815629f62f7958254ce181a4254ccdb517
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-19 07:24:07 +0530

    Update sum.h

--- inc/sum.h
+++ inc/sum.h
@@ -4,6 +4,6 @@
 int sum(int number1, int number2);
 int diff(int number1, int number2);
 int mul(int number1, int number2);
-int div(int number1, int number2);
+int division(int number1, int number2);
 int mod(int number1, int number2);
 #endif
