1c14d9687f1b8e05f1fe0807bf52f101f7db2c2b
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-19 07:22:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 		printf("Sum = %d",  sum(10, 20));
 		printf("Difference = %d",  diff(10, 20));
 		printf("Multiplication= %d",  mul(10, 20));
-		printf("division = %d",  div(10, 20));
+		printf("division = %d",  division(10, 20));
 		printf("mod = %d",  mod(10, 20));
 
 								
