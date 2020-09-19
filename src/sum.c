ad23500b733ba10f5aca57906697cb16d60e8bc0
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-19 07:33:57 +0530

    Update sum.c

--- src/sum.c
+++ src/sum.c
@@ -18,3 +18,15 @@ int mod(int number1, int number2)
 	{
 		return number1 % number2;
 	}
+
+int prime(int n){
+    int c=0;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0) c++;
+
+    }
+    if(c==0) return 1;
+    else return 0;
+
+}
