1f409dd4654f3bf307529df2414127d0b58cac97
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-19 07:32:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,29 @@
 	int main()
 	{
 
-							
+		int n;					
+	        //sum
 		printf("Sum = %d",  sum(10, 20));
+		//diff
 		printf("Difference = %d",  diff(10, 20));
+		//mul
 		printf("Multiplication= %d",  mul(10, 20));
+		//div
 		printf("division = %d",  division(10, 20));
+		//mod
 		printf("mod = %d",  mod(10, 20));
-
-								
+		//prime or not 
+                printf("Enter a number");
+		scanf("%d",&n);
+		if(n==1||n==0){printf("%d is not a prime number",n ); exit;}
+                int y=prime(n);
+                if(y==1) printf("%d is a prime number",n);
+                else printf("%d is not a prime number",n);
+                return 0;
+		
+		//
+		if(n%2==0) print("Even number");
+		else print("odd");
 
 		return 0;
 	}
